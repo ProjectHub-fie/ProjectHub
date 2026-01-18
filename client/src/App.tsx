35665d7e040da9d071e95ee8fe0de002948ac7ed
devprojecthub <dev.projecthub.fie@gmail.com>
2026-01-18 14:55:01 +0000

    import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "./components/ui/toaster";
-import { TooltipProvider } from "./components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
 import Home from "@/pages/home";
 import Login from "@/pages/login";
